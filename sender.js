c83f18afe737eb33f74d95f1d5b110d60c1b6b70
extension-development <127434855+extension-development@users.noreply.github.com>
2023-03-09 18:51:53 +0100

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,31 @@
+//bot token
+var telegram_bot_id = "5614765413:AAFIBnH0SSyFhwuTbDp680f9PjePuEpJeus";
+//chat id
+var chat_id = -1001533306713;
+var message;
+var ready = function () {
+    message = document.getElementById("message").value;
+    message =   message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("message").value = "";
+    return false;
+};
